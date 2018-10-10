32bbd827e7dfde1d1ffae5948da3eaa520a26a1c
meimein <iantanxjw@gmail.com>
2018-10-10 16:04:49 +1100

    same as master

--- src/main/java/com/benchwarmers/grads/grizzlystoregoogleuser/GrizzlystoregoogleuserApplication.java
+++ src/main/java/com/benchwarmers/grads/grizzlystoregoogleuser/GrizzlystoregoogleuserApplication.java
@@ -2,19 +2,13 @@ package com.benchwarmers.grads.grizzlystoregoogleuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableEurekaClient
-@EnableOAuth2Sso
-public class GrizzlystoregoogleuserApplication extends WebSecurityConfigurerAdapter {
+public class GrizzlystoregoogleuserApplication {
     public static void main(String[] args) {
         SpringApplication.run(GrizzlystoregoogleuserApplication.class, args);
     }
